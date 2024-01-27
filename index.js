7acd7ed3bc156f1b437d479ad3ea1d6123a63c8a
Jitendra <jitendra_vr@hotmail.com>
2024-01-27 01:48:08 -0500

    Added typeDefs & Resolvers

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 
+//db
+import db from './_db.js'
+
+//types
+import { typeDefs } from './schema.js'
+
+const resolvers = {
+    Query: {
+        games(){
+            return db.games
+        },
+        reviews(){
+            return db.reviews
+        },
+        authors(){
+            return db.authors
+        },
+    }
+}
+
 // Server
 const server = new ApolloServer({
     //typeDefs -- definitions of data types
-    //resolvers
+    typeDefs,
+     //resolvers -- handle incoming requests and return data to the clients
+    resolvers
 
 })
 
 const { url } = await startStandaloneServer(server, {
     listen: {port: 4000}
+
 })
 
 console.log('Server running at port', 4000)
