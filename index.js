251a375c4952419ce9fb67c61c7f03907bf65dd4
Jitendra <jitendra_vr@hotmail.com>
2024-01-27 19:31:42 -0500

    Created resolvers for relational data

--- index.js
+++ index.js
@@ -12,22 +12,40 @@ const resolvers = {
         games() {
             return db.games
         },
-        reviews() {
-            return db.reviews
+        game(_, args) {
+            return db.games.find((game) => game.id === args.id)
         },
         authors() {
             return db.authors
         },
+        author(_, args) {
+            return db.authors.find((author) => author.id === args.id)
+        },
+        reviews() {
+            return db.reviews
+        },
         review(_, args) {
             return db.reviews.find((review) => review.id === args.id)
         },
-        game(_, args) {
-            return db.games.find((game) => game.id === args.id)
-        },
-        authors(_, args) {
-            return db.authors.find((author) => author.id === args.id)
+    },
+    Game: {
+        reviews(parent) {
+          return db.reviews.filter((r) => r.game_id === parent.id)
         }
-    }
+      },
+      Review: {
+        author(parent) {
+          return db.authors.find((a) => a.id === parent.author_id)
+        },
+        game(parent) {
+          return db.games.find((g) => g.id === parent.game_id)
+        }
+      },
+      Author: {
+        reviews(parent) {
+          return db.reviews.filter((r) => r.author_id === parent.id)
+        }
+      },
 }
 
 // Server
@@ -44,4 +62,4 @@ const { url } = await startStandaloneServer(server, {
 
 })
 
-console.log('Server running at port', 4000)
+console.log(`Server running at: ${url}`)
